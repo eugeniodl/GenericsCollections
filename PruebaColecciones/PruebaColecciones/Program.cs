6f31d426a9570ef268d79f9eb82c062be0b8ce51
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-10-10 22:23:34 -0600

    Prueba Colecciones

---
+++ PruebaColecciones/PruebaColecciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+
+// Cree una colección de frutas
+Collection<string> frutas = new Collection<string>();
+frutas.Add("Limón");
+frutas.Add("Manzana");
+frutas.Add("Fresa");
+frutas.Add("Pitahaya");
+frutas.Add("Sandía");
+
+// Imprimir la colección
+Display(frutas);
+
+// Insertar un elemento a la colección
+frutas.Insert(3, "Uva");
+Display(frutas);
+
+// Verificar si en la colección está la naranja
+Console.WriteLine($"La colección contiene la naranja: " +
+    $"{frutas.Contains("Naranja")}");
+
+// Remover el índice 2 de la colección
+//frutas.Remove("Uva");
+frutas.RemoveAt(2);
+
+// Imprimir la colección
+Display(frutas);
+
+// Contar elementos de la colección
+Console.WriteLine($"La colección tiene {frutas.Count} elementos");
+
+void Display(Collection<string> frutas)
+{
+    foreach (var fruta in frutas)
+    {
+        Console.WriteLine(fruta);
+    }
+    Console.WriteLine("---------------------------");
+}
---
+++ PruebaColecciones/PruebaList/Program.cs
@@ -0,0 +1,52 @@
+﻿string[] colores =
+    { "MAGENTA", "ROJO", "BLANCO", "AZUL", "CYAN"};
+string[] eliminarColores =
+    { "ROJO", "BLANCO", "AZUL" };
+
+List<string> lista = new List<string>();
+
+Console.WriteLine($"Capacidad de la lista {lista.Capacity}");
+
+foreach (var color in colores)
+{
+    lista.Add(color);
+}
+
+List<string> eliminarLista = new List<string>(eliminarColores);
+
+MostrarInformacion(lista);
+
+EliminarColores(lista, eliminarColores);
+
+void EliminarColores(List<string> lista, string[] eliminarColores)
+{
+    for (int i = 0; i < eliminarColores.Length; i++)
+    {
+        lista.Remove(eliminarColores[i]);
+    }
+}
+
+MostrarInformacion(lista);
+
+void MostrarInformacion(List<string> lista)
+{
+    foreach (var item in lista)
+    {
+        Console.Write($"{item} ");
+    }
+
+    Console.WriteLine($"\nTamaño = {lista.Count}; " +
+        $"Capacidad = {lista.Capacity}");
+
+    int index = lista.IndexOf("AZUL");
+
+    if(index != -1)
+        Console.WriteLine($"La lista contiene AZUL en el índice " +
+            $"{index}");
+    else
+        Console.WriteLine("La lista no contiene AZUL");
+}
+
+// Elimine de primera Lista los colores especificados
+// en la segunda Lista
+
