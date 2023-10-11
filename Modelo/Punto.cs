c2199a2f05d653c5f43490d9a11032a6c8aa8de3
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-10-11 14:55:52 -0600

    Prueba Queue

---
+++ Modelo/Punto.cs
@@ -0,0 +1,19 @@
+﻿namespace Modelo
+{
+    public class Punto
+    {
+        private int _x;
+        private int _y;
+
+        public Punto(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"X={_x}, Y={_y}");
+        }
+    }
+}
---
+++ PruebaQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using Modelo;
+
+Queue<Punto> puntos = new Queue<Punto>();
+
+puntos.Enqueue(new Punto(3, 7));
+puntos.Enqueue(new Punto(7, 9));
+puntos.Enqueue(new Punto(11, 2));
+puntos.Enqueue(new Punto(5, 8));
+
+Imprimir(puntos);
+
+void Imprimir<T>(Queue<T> collection)
+{
+    foreach (T item in collection)
+    {
+        Console.Write($"{item} ");
+        Console.Write(item + " ");
+        Console.Write("{0} ", item);
+    }
+}
+
