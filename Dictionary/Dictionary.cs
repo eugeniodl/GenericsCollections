851a405954384330e7a557fcaed557ba6282a9d1
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-10-17 09:41:37 -0600

    Ejemplo Dictionary

---
+++ Dictionary/Dictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public static class Dictionary
+    {
+        private static Dictionary<string, string> _dictionary = 
+            new Dictionary<string, string>
+        {
+                { "mouse", "A computer mouse is a handheld hardware input device that controls a cursor in a GUI (graphical user interface) and can move and select text." },
+                { "keyboard", "A keyboard is one of the primary input devices used with a computer. Similar to an electric typewriter, a keyboard is composed of buttons" },
+                { "laptop", "A laptop, laptop computer, or notebook computer is a small, portable personal computer with a screen and alphanumeric keyboard."},
+                { "Java", "Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible." },
+                { "c++", "C++ is a powerful general-purpose programming language. It can be used to develop operating systems, browsers, games, and so on." },
+                { "python", "Python is an interpreted high-level general-purpose programming language. Its design philosophy emphasizes code readability." },
+                { "html", "The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser." },
+                { "css", "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML." },
+                { "programming", "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing result." },
+        };
+
+        public static string GetMeaning(string word)
+        {
+            try
+            {
+                return _dictionary[word];
+            }
+            catch
+            {
+                return "Sorry! Word Not Found.";
+            }
+        }
+    }
+}
---
+++ Dictionary/frmDictionary.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Dictionary
+{
+    partial class frmDictionary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(276, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dictionary";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSearch.Location = new System.Drawing.Point(94, 81);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(201, 35);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSearch.Location = new System.Drawing.Point(430, 81);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(122, 35);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtResult.Location = new System.Drawing.Point(47, 157);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(581, 257);
+            this.txtResult.TabIndex = 3;
+            // 
+            // frmDictionary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDictionary";
+            this.Text = "Dictionary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtSearch;
+        private Button btnSearch;
+        private TextBox txtResult;
+    }
+}
