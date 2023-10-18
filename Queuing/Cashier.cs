af7aec95d41b0ba8b6b932d265d9660eee22ccfb
eugeniodl <Eugenio.diaz@fcys.uni.edu.ni>
2023-10-18 09:54:04 -0600

    Prueba Queue

---
+++ Queuing/Cashier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queuing
+{
+    internal class Cashier
+    {
+        private int _count = 0;
+        public static string getNumberInQueue = "";
+        public static Queue<string> cashierQueue;
+
+        public Cashier()
+        {
+            _count = 10000;
+            cashierQueue = new Queue<string>();
+        }
+
+        public string CashierGeneratedNumber(string cashierNumber)
+        {
+            _count++;
+            cashierNumber += _count.ToString();
+            return cashierNumber;
+        }
+    }
+}
---
+++ Queuing/frmQueuing.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Queuing
+{
+    partial class frmQueuing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCashier = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPosQueue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCashier
+            // 
+            this.btnCashier.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCashier.Location = new System.Drawing.Point(37, 12);
+            this.btnCashier.Name = "btnCashier";
+            this.btnCashier.Size = new System.Drawing.Size(120, 120);
+            this.btnCashier.TabIndex = 0;
+            this.btnCashier.Text = "Cashier";
+            this.btnCashier.UseVisualStyleBackColor = true;
+            this.btnCashier.Click += new System.EventHandler(this.btnCashier_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(37, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "*Click to get a number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(195, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Position in Queue";
+            // 
+            // lblPosQueue
+            // 
+            this.lblPosQueue.AutoSize = true;
+            this.lblPosQueue.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPosQueue.Location = new System.Drawing.Point(195, 67);
+            this.lblPosQueue.Name = "lblPosQueue";
+            this.lblPosQueue.Size = new System.Drawing.Size(145, 40);
+            this.lblPosQueue.TabIndex = 3;
+            this.lblPosQueue.Text = "                ";
+            // 
+            // frmQueuing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 172);
+            this.Controls.Add(this.lblPosQueue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCashier);
+            this.Name = "frmQueuing";
+            this.Text = "QueuingForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnCashier;
+        private Label label1;
+        private Label label2;
+        private Label lblPosQueue;
+    }
+}
