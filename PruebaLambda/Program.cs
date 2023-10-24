5c607345c4ec2c69e170db7aa0c1d925278fb974
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-10-24 11:12:51 -0600

    Prueba Lambda

---
+++ PruebaLambda/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using PruebaLambda;
+
+List<Client> clients = new List<Client>()
+{
+    new Client()
+    {
+        ID = 1,
+        Name = "Emanuel Reyes",
+    },
+    new Client()
+    {
+        ID = 2,
+        Name = "Rosa Ordoñez",
+    },
+    new Client()
+    {
+        ID = 3,
+        Name = "Freddy García",
+    },
+};
+
+var client = clients.Find((Client c) =>
+{
+    return c.ID == 2;
+});
+
+if (client != null)
+    Console.WriteLine(client.Name);
+else
+    Console.WriteLine("Client Not Found");
