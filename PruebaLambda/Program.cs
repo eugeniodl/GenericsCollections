61b9037546aa4daf282f859d1698bb0e5c39feef
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-10-25 11:32:24 -0600

    Prueba Lambda

--- PruebaLambda/Program.cs
+++ PruebaLambda/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-using PruebaLambda;
+﻿using PruebaLambda;
 
 List<Client> clients = new List<Client>()
 {
@@ -21,12 +19,57 @@ List<Client> clients = new List<Client>()
     },
 };
 
+// Hacemos uso de expresiones Lambda para encontrar
+// cliente con ID igual a 2
+
 var client = clients.Find((Client c) =>
 {
     return c.ID == 2;
 });
 
+var client2 = clients.Find(c => c.ID == 3);
+
 if (client != null)
     Console.WriteLine(client.Name);
 else
     Console.WriteLine("Client Not Found");
+
+
+Client client3 = new Client();
+client3.ID = 4;
+client3.Name = "Pedro Tórrez";
+
+Client client4 = new Client();
+client4.ID = 5;
+client4.Name = "Telma Bello";
+
+CompareClient compareClient = (c1, c2) =>
+c1 == c2;
+
+Console.WriteLine(compareClient(client3.Name, 
+    client4.Name));
+
+List<int> numeros = new List<int>();
+numeros.AddRange(Enumerable.Range(0, 100));
+
+//Hacemos uso de expresiones lambda
+//para obtener números pares
+var numPares = numeros.FindAll(x => x % 2 == 0);
+
+foreach (var num in numPares)
+{
+    Console.Write("{0,1} ", num);
+}
+
+numeros.ForEach(x => Console.Write("{0,1} ", x));
+
+//Haciendo uso de expresiones lambda
+//obtenga números primos
+
+
+delegate bool CompareClient(string n1, string n2);
+
+
+
+
+
