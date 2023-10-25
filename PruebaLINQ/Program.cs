e15af2706ca032255ce99ed198019df2d8076bd4
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-10-25 10:02:52 -0600

    Prueba LINQ

---
+++ PruebaLINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// 1. Data source.
+using System;
+
+int[] numeros = Enumerable.Range(1, 100).ToArray();
+
+// 2. Query creation.
+IEnumerable<int> numPares =
+    from num in numeros
+    where num % 2 == 0
+    select num;
+
+// 3. Query execution.
+foreach (var num in numPares)
+{
+    Console.Write("{0,1} ", num);
+}
--- PruebaLambda/Program.cs
+++ PruebaLambda/Program.cs
@@ -41,7 +41,8 @@ else
 Console.WriteLine("-------------------------");
 
 List<int> numeros = new List<int>();
-numeros.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+numeros.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
+    14});
 
 // Hacemos uso de la expresión lambda para encontrar números pares
 List<int> numPares = numeros.FindAll(x => (x % 2) == 0);
@@ -50,3 +51,42 @@ foreach (var item in numPares)
 {
     Console.WriteLine(item);
 }
+
+// Hacemos uso de la expresión lambda para encontrar números primos
+List<int> numPrimos = numeros.FindAll(n =>
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+
+
+    return true;
+});
+
+numPrimos.ForEach(x =>  Console.Write(x));
+
+
+numPares.ForEach(n =>
+{
+    Console.WriteLine("El número par es: ");
+    Console.WriteLine(n);
+});
+
+Client cliente4 = new Client();
+cliente4.ID = 4;
+cliente4.Name = "Pablo";
+
+Client cliente5 = new Client();
+cliente5.ID = 5;
+cliente5.Name = "Luisa";
+
+ComparaClientes comparaNombre = (c1, c2) => 
+c1 == c2;
+
+Console.WriteLine(comparaNombre(cliente4.Name, cliente5.Name));
+
+delegate bool ComparaClientes(string n1, string n2);
